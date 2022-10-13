6e249988e6bbedc0eae0dc843d05beb7b6ef8c32
minw <ishin4554@gmail.com>
2022-10-13 22:14:50 +0800

    REFACTOR 替測試新增 beforeEach 來共用 fixture

--- __test__/index.test.mjs
+++ __test__/index.test.mjs
@@ -2,12 +2,16 @@ import { expect } from 'chai';
 import { Province } from '../src/Province.mjs';
 import sampleProvinceData from "../src/index.mjs";
 describe('province', function () {
+  // 不要這樣做：共用的 fixture 會讓不同測試間相互影響
+  // const asia = new Province(sampleProvinceData());
+  let asia;
+  beforeEach(function() {
+    asia = new Province(sampleProvinceData());
+  })
   it('should get shortfall', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
   it('should get profit', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
