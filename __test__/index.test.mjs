af23f15d01e78be5c7771195429d0c3398417f5b
minw <ishin4554@gmail.com>
2022-10-13 22:02:50 +0800

    UPDATE 使用 chai 更改 assert style 為 expect style 測試風格

--- __test__/index.test.mjs
+++ __test__/index.test.mjs
@@ -1,9 +1,9 @@
-import assert from 'assert';
+import { expect } from 'chai';
 import { Province } from '../src/Province.mjs';
 import sampleProvinceData from "../src/index.mjs";
 describe('province', function () {
   it('should get shortfall', function () {
     const asia = new Province(sampleProvinceData());
-    assert.equal(asia.shortfall, 5);
+    expect(asia.shortfall).equal(5);
   });
 });
