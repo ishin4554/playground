d3ace53c5392e973f79d13fd2ad350ebe2470667
minw <ishin4554@gmail.com>
2022-11-10 22:14:36 +0800

    REFACTOR ex1-1 encapsulate var

--- 01-encapsulate_record/index.js
+++ 01-encapsulate_record/index.js
@@ -3,8 +3,18 @@ const organization = {
   country: "GB"
 }
 
+// 1. encapsulate variable
+// 命名先很醜也容易搜尋，因為是過度步驟
+export const getRawDataOfOrganization = () => {
+  return organization
+}
+
+const setRawDataOfOrganization = (newName) => {
+  getRawDataOfOrganization().name = newName;
+}
+
 export const render = () => {
   let result = ''
-  result += `<h1>${organization.name}</h1>`
+  result += `<h1>${getRawDataOfOrganization().name}</h1>`
   return result
 }
