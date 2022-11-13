01a0bde1000078c2fd4dd6cf66e374f107b1381a
minw <ishin4554@gmail.com>
2022-11-13 18:21:07 +0800

    REFACTOR ex1-1 create telephoneNumber class & move field

--- 05-extract_class/index.js
+++ 05-extract_class/index.js
@@ -1,27 +1,44 @@
-export class Person {
-  constructor() {
-    this._name = '';
-    this._officeAreaCode = '';
-    this._officeNumber = '';
-  }
-  get name() {
-    return this._name;
-  }
-  set name(arg) {
-    this._name = arg;
-  }
-  get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
-  }
+class TelephoneNumber {
   get officeAreaCode() {
     return this._officeAreaCode;
   }
+
   set officeAreaCode(arg) {
     this._officeAreaCode = arg;
   }
+}
+
+export class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+    this._name = '';
+    this._officeNumber = '';
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(arg) {
+    this._name = arg;
+  }
+
+  get telephoneNumber() {
+    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+  }
+
+  get officeAreaCode() {
+    return this._telephoneNumber.officeAreaCode;
+  }
+
+  set officeAreaCode(arg) {
+    this._telephoneNumber.officeAreaCode = arg;
+  }
+
   get officeNumber() {
     return this._officeNumber;
   }
+
   set officeNumber(arg) {
     this._officeNumber = arg;
   }
