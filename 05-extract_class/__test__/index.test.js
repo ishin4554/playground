48b839bc8db4719e6223f984e25fff424d6b09a7
minw <ishin4554@gmail.com>
2022-11-13 18:17:47 +0800

    ADD extract class example

---
+++ 05-extract_class/__test__/index.test.js
@@ -0,0 +1,10 @@
+import {Person} from "../index";
+
+describe('extract class', function () {
+  test('should be telephoneNumber', function () {
+    const person = new Person();
+    person.officeAreaCode = '03';
+    person.officeNumber = '54545454';
+    expect(person.telephoneNumber).toBe('(03) 54545454')
+  });
+});
