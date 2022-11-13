06af680b6fd3e3aba88f85036368bb38da4afd5e
minw <ishin4554@gmail.com>
2022-11-13 16:08:12 +0800

    REFACTOR step 2 create simple class Priority and rename function

--- 03-replace_primitive_with_object/__test__/index.test.js
+++ 03-replace_primitive_with_object/__test__/index.test.js
@@ -1,7 +1,7 @@
 import {getHighPriorityCount} from "../index";
 
 describe('replace primitive with object', function () {
-  test('should be high priority count', function () {
+  test('should be high priorityString count', function () {
     expect(getHighPriorityCount()).toBe(2)
   });
 });
--- 03-replace_primitive_with_object/index.js
+++ 03-replace_primitive_with_object/index.js
@@ -1,14 +1,26 @@
 class Order {
   constructor(data) {
-    this._priority = data.priority;
+    this.priority = data.priority;
   }
 
-  get priority() {
-    return this._priority;
+  // 因為 Priority getter 為 toString 故 function 也調整為 priorityString()
+  get priorityString() {
+    return this._priority.toString();
   }
 
   set priority(aString) {
-    this._priority = aString;
+    this._priority = new Priority(aString);
+  }
+}
+
+class Priority {
+  constructor(value) {
+    this._value = value;
+  }
+
+  // 作者認為對使用方來說，取得字串比較像是轉換而非取得屬性。
+  toString() {
+    return this._value;
   }
 }
 
@@ -16,5 +28,5 @@ const rawOrders = [{priority: 'high'}, {priority: 'normal'}, {priority: 'rush'}]
 const orders = rawOrders.map(o => new Order(o))
 
 export function getHighPriorityCount() {
-  return orders.filter(o => "high" === o.priority || "rush" === o.priority).length;
+  return orders.filter(o => "high" === o.priorityString || "rush" === o.priorityString).length;
 }
