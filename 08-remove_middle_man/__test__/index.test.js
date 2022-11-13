52f34dfc414a85f4b232c50d1d19ab6e9a51b082
minw <ishin4554@gmail.com>
2022-11-13 21:42:38 +0800

    ADD remove middle man

---
+++ 08-remove_middle_man/__test__/index.test.js
@@ -0,0 +1,11 @@
+import {Department, Person} from "../index";
+
+describe('remove middle man', function () {
+  test('should not delegate', function () {
+    const person = new Person('David');
+    const department = new Department();
+    department.manager = 'Lucy';
+    person.department = department;
+    expect(person.manager).toBe('Lucy');
+  });
+});
---
+++ 08-remove_middle_man/index.js
@@ -0,0 +1,39 @@
+export class Person {
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._department;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  set department(arg) {
+    this._department = arg;
+  }
+
+  get manager() {
+    return this._department.manager;
+  }
+}
+
+export class Department {
+  get chargeCode() {
+    return this._chargeCode;
+  }
+
+  set chargeCode(arg) {
+    this._chargeCode = arg;
+  }
+
+  get manager() {
+    return this._manager;
+  }
+
+  set manager(arg) {
+    this._manager = arg;
+  }
+}
