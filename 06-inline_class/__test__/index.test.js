8649784fff54637fcb7e163741e48c89805acd9b
minw <ishin4554@gmail.com>
2022-11-13 21:27:25 +0800

    REFACTOR ex1-3 partial move field

--- 06-inline_class/__test__/index.test.js
+++ 06-inline_class/__test__/index.test.js
@@ -1,9 +1,8 @@
-import {Shipment, TrackingInformation} from "../index";
+import {Shipment} from "../index";
 
 describe('inline class', function () {
   test('should be trackingInfo', function () {
     const shipment = new Shipment();
-    shipment.trackingInformation = new TrackingInformation();
     shipment.shippingCompany = 'looplus';
     shipment.trackingNumber = 1001;
     expect(shipment.trackingInfo).toBe('looplus: 1001');
--- 06-inline_class/index.js
+++ 06-inline_class/index.js
@@ -1,49 +1,22 @@
-export class TrackingInformation {
-  get shippingCompany() {
-    return this._shippingCompany;
+export class Shipment {
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`
   }
 
   set shippingCompany(arg) {
-    this._shippingCompany = arg
+    this._shippingCompany = arg;
   }
 
-  get trackingNumber() {
-    return this._trackingNumber;
+  get shippingCompany() {
+    return this._shippingCompany;
   }
 
   set trackingNumber(arg) {
     this._trackingNumber = arg;
   }
 
-}
-
-export class Shipment {
-  get trackingInfo() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`
-  }
-
-  get trackingInformation() {
-    return this._trackingInformation;
-  }
-
-  set trackingInformation(aTrackingInformation) {
-    this._trackingInformation = aTrackingInformation;
-  }
-
-  set shippingCompany(arg) {
-    this._trackingInformation.shippingCompany = arg;
-  }
-
-  get shippingCompany() {
-    return this._trackingInformation.shippingCompany;
-  }
-
-  set trackingNumber(arg) {
-    this._trackingInformation.trackingNumber = arg;
-  }
-
   get trackingNumber() {
-    return this._trackingInformation.trackingNumber;
+    return this._trackingNumber;
   }
 }
 
