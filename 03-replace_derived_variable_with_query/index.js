6334c70f0a6776c867a9492cdbdf23badd6ca87c
minw <ishin4554@gmail.com>
2022-12-11 21:50:11 +0800

    REFACTOR remove dead code

--- 03-replace_derived_variable_with_query/index.js
+++ 03-replace_derived_variable_with_query/index.js
@@ -1,15 +1,14 @@
 class ProductionPlan {
   constructor() {
-    this._production = 0;
     this._adjustments = []
   }
 
   get production() {
     return this._adjustments.reduce((sum, a) => sum + a.amount, 0)
   }
+
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment)
-    this._production += anAdjustment.amount;
   }
 }
 
