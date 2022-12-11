f7dd6459e5f8d79676142baf023f4074683c367f
minw <ishin4554@gmail.com>
2022-12-12 00:11:49 +0800

    REFACTOR change value to reference by repository

--- 04-change_value_to_reference/index.js
+++ 04-change_value_to_reference/index.js
@@ -1,7 +1,23 @@
+let _repositoryData;
+
+export const initialize = () => {
+  _repositoryData = {};
+  _repositoryData.customers = new Map();
+}
+
+export const findCustomer = (id) => {
+  return _repositoryData.customers.get(id)
+}
+
+export const registerCustomer = (id) => {
+  if (!_repositoryData.customers.has(id)) _repositoryData.customers.set(id, new Customer(id))
+  return findCustomer(id);
+}
+
 class Order {
   constructor(data) {
     this._number = data.number;
-    this._customer = new Customer(data.customer);
+    this._customer = registerCustomer(data.customer)
   }
 
   get customer() {
