796f83017878f3c13d9bd1f48ca29664307bbff3
minw <ishin4554@gmail.com>
2022-12-11 21:52:55 +0800

    ADD update to example 2

--- 03-replace_derived_variable_with_query/__tests__/index.test.js
+++ 03-replace_derived_variable_with_query/__tests__/index.test.js
@@ -2,8 +2,8 @@ import ProductionPlan from "../index";
 
 describe('replace derived variable with query', function () {
   test('should get production', function () {
-    const plan = new ProductionPlan()
+    const plan = new ProductionPlan(20)
     plan.applyAdjustment({amount: 20})
-    expect(plan.production).toBe(20);
+    expect(plan.production).toBe(40);
   });
 });
