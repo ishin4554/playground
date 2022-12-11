5b9b485fc54c46c820c5ab7b22c1627991c51ecb
minw <ishin4554@gmail.com>
2022-12-11 20:37:50 +0800

    REFACTOR split variable

--- 01-split_variable/ex2.js
+++ 01-split_variable/ex2.js
@@ -1,5 +1,5 @@
-const discount = (inputValue, quantity) => {
-  // 如果買超過 100 個，少一塊，如果買價格超過 50 少 2 塊
+const discount = (originalValue, quantity) => {
+  let inputValue = originalValue;
   if (inputValue > 50) inputValue = inputValue - 2;
   if (quantity > 100) inputValue = inputValue - 1;
   return inputValue;
