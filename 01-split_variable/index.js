32c1994ff51dacfd9ed2a81b236d973a45142043
minw <ishin4554@gmail.com>
2022-12-11 20:25:49 +0800

    REFACTOR rename 有被重新賦值的變數

--- 01-split_variable/index.js
+++ 01-split_variable/index.js
@@ -1,20 +1,16 @@
 // 計算一個物體受力後的距離，其中物體會受力兩次，中間會經過一段延遲。
 const distanceTraveled = (scenario, time) => {
- let result;
- // F = ma 反之 a = F/m
- let acc = scenario.primaryForce / scenario.mass;
- let primaryTime = Math.min(time, scenario.delay);
- // s = 1/2*a*t^2 取得移動距離
- result = 0.5 * acc * primaryTime * primaryTime;
- // 如果總時間 > 延遲時間，則計算第二段的加速
- let secondaryTime = time - scenario.delay;
- if(secondaryTime > 0) {
-   // 初速度 v = at
-   let primaryVelocity = acc * scenario.delay;
-   // 第二次加速的加速度重新賦值
-   acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-   result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
- }
- return result;
+  let result;
+  const primaryAcceleration = scenario.primaryForce / scenario.mass;
+  let primaryTime = Math.min(time, scenario.delay);
+  result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
+  let secondaryTime = time - scenario.delay;
+  if (secondaryTime > 0) {
+    let primaryVelocity = primaryAcceleration * scenario.delay;
+    // 在下一次賦值時宣告變數
+    let acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+  }
+  return result;
 }
 export default distanceTraveled;
