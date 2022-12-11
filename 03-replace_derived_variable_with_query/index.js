049cba3e79defc039d3f19714db66a63121ef16c
minw <ishin4554@gmail.com>
2022-12-11 21:58:03 +0800

    REFACTOR introduce assertion

--- 03-replace_derived_variable_with_query/index.js
+++ 03-replace_derived_variable_with_query/index.js
@@ -6,7 +6,12 @@ class ProductionPlan {
   }
 
   get production() {
+    console.assert(this._productionAccumulator === this.calculatedProductionAccumulator)
     return this._initialProduction + this._productionAccumulator;
+  }
+// 作者在這裡傾向保留這個屬性，而不是 inline，猜測是因為這個屬性有可能被公開，或是 reduce 方法並不是這麼直覺
+  get calculatedProductionAccumulator() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0)
   }
 
   applyAdjustment(anAdjustment) {
