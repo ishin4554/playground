472eb9239342590414f0affdb0762686a860e2fa
minw <ishin4554@gmail.com>
2022-12-11 21:55:27 +0800

    REFACTOR split variable

--- 03-replace_derived_variable_with_query/index.js
+++ 03-replace_derived_variable_with_query/index.js
@@ -1,16 +1,17 @@
 class ProductionPlan {
   constructor(production) {
-    this._production = production;
+    this._initialProduction = production;
+    this._productionAccumulator = 0;
     this._adjustments = []
   }
 
   get production() {
-    return this._production;
+    return this._initialProduction + this._productionAccumulator;
   }
 
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment)
-    this._production += anAdjustment.amount;
+    this._productionAccumulator += anAdjustment.amount;
   }
 }
 
