b2bac097c5f238ce4c781357cf919c1c45b4370d
minw <ishin4554@gmail.com>
2022-12-11 23:18:21 +0800

    REFACTOR reassignment the setter

--- 04-change_reference_to_value/index.js
+++ 04-change_reference_to_value/index.js
@@ -8,16 +8,8 @@ class TelephoneNumber {
     return this._areaCode;
   }
 
-  set areaCode(arg) {
-    this._areaCode = arg;
-  }
-
   get number() {
     return this._number;
-  }
-
-  set number(arg) {
-    this._number = arg;
   }
 }
 
