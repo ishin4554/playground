c59332c52b6362c855bbd0180bc67741a0762ae6
minw <ishin4554@gmail.com>
2023-01-13 13:52:04 +0800

    ADD init preserve whole object

---
+++ 04-preserve_whole_object/__tests__/index.test.js
@@ -0,0 +1,22 @@
+import HeatingPlan from "../index";
+
+let alerts = [];
+const addAlert = (aRoom, aPlan) => {
+  const low = aRoom.daysTempRange.low;
+  const high = aRoom.daysTempRange.high;
+  if (!aPlan.withinRange(low, high)) alerts.push("room temperature went outside range");
+}
+
+describe('preserve whole project', function () {
+  test('should be example 1', function () {
+    const room = {
+      daysTempRange: {
+        low: 10,
+        high: 40,
+      }
+    }
+    const plan = new HeatingPlan()
+    addAlert(room, plan);
+    expect(alerts).toEqual(["room temperature went outside range"])
+  });
+});
