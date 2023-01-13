894503644764c87448cd6c7ac1fe9ab5cabde41d
minw <ishin4554@gmail.com>
2023-01-13 12:31:13 +0800

    ADD init parameterize function project example 2

---
+++ 02-parameterize_function/__tests__/index.test.js
@@ -0,0 +1,7 @@
+import baseCharge from "../index";
+
+describe('parameterize function', function () {
+  test('should be example 2', function () {
+    expect(baseCharge(20)).toBe(0.198)
+  });
+});
--- 02-parameterize_function/index.js
+++ 02-parameterize_function/index.js
@@ -1,12 +1,22 @@
-const tenPercentRaise = (aPerson) => {
-  aPerson.salary = aPerson.salary.multiply(1.1);
+const usd = (val) => val * 0.33;
+const baseCharge = (usage) => {
+  if(usage < 0) return usd(0);
+  const amount = bottomBand(usage) * 0.03
+    + middleBand(usage) * 0.05
+  + topBand(usage) * 0.07;
+  return usd(amount);
 }
 
-const fivePercentRaise = (aPerson) => {
-  aPerson.salary = aPerson.salary.multiply(1.05);
+const bottomBand = (usage) => {
+  return Math.min(usage, 100);
 }
 
-const raise = (aPerson, factor) => {
-  aPerson.salary = aPerson.salary.multiply(1 + factor);
+const middleBand = (usage) => {
+  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
 }
 
+const topBand = (usage) => {
+  return usage > 200 ? usage - 200 : 0;
+}
+
+export default baseCharge;
