aeb63e157edf0a8e0740cb5f84d5d29d0b3eae82
minw <ishin4554@gmail.com>
2023-01-13 14:18:22 +0800

    REFACTOR change function declaration

--- 05-replace_parameter_with_query/index.js
+++ 05-replace_parameter_with_query/index.js
@@ -10,10 +10,10 @@ class Order {
 
   get finalPrice() {
     const basePrice = this.quantity * this.itemPrice;
-    return this.discountedPrice(basePrice, this.discountLevel);
+    return this.discountedPrice(basePrice);
   }
 
-  discountedPrice(basePrice, discountLevel) {
+  discountedPrice(basePrice) {
     switch (this.discountLevel) {
       case 1 :
         return basePrice * 0.95;
