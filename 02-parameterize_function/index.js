5ff506dbf9c193242409cdf903bbe8146a2891e7
minw <ishin4554@gmail.com>
2023-01-13 12:32:42 +0800

    UPDATE change constant to parameter

--- 02-parameterize_function/index.js
+++ 02-parameterize_function/index.js
@@ -2,13 +2,13 @@ const usd = (val) => val * 0.33;
 const baseCharge = (usage) => {
   if (usage < 0) return usd(0);
   const amount = bottomBand(usage) * 0.03
-    + withinBand(usage) * 0.05
+    + withinBand(usage, 100, 200) * 0.05
     + topBand(usage) * 0.07;
   return usd(amount);
 }
 
 const withinBand = (usage, bottom, top) => {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0
+  return usage > bottom ? Math.min(usage, top) - bottom : 0
 }
 
 const bottomBand = (usage) => {
