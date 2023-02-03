39e7f89503471a50b2df83601ca89fd5ef5098e3
minw <ishin4554@gmail.com>
2023-02-03 14:02:02 +0800

    REFACTOR inline function

--- 07-remove_setting_method/index.js
+++ 07-remove_setting_method/index.js
@@ -1,12 +1,20 @@
 class Person {
   constructor(id) {
     this._name = '';
-    this.id = id;
+    this._id = id;
   }
-  get name() { return this._name; }
-  set name(arg) { return this._name = arg; }
-  get id() { return this._id; }
-  set id(arg) { this._id = arg; }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(arg) {
+    return this._name = arg;
+  }
+
+  get id() {
+    return this._id;
+  }
 }
 
 export default Person;
