ba04df47c4baecdf4944d6e2c265433a2b7f5f0d
minw <ishin4554@gmail.com>
2023-02-03 15:10:58 +0800

    REFACTOR extract function

--- 10-replace_command_with_function/__tests__/index.test.js
+++ 10-replace_command_with_function/__tests__/index.test.js
@@ -1,11 +1,11 @@
-import ChargeCalculate from "../index";
+import charge from "../index";
 
 describe('replace command with function', function () {
   test('should be charge', function () {
-    const customer = { baseRate: 1.1 }
+    const customer = {baseRate: 1.1}
     const usage = 10
-    const provider = { connectionCharge: 10 }
-    const monthCharge = new ChargeCalculate(customer, usage, provider).charge;
+    const provider = {connectionCharge: 10}
+    const monthCharge = charge(customer, usage, provider);
     expect(monthCharge).toBe(21);
   });
 });
--- 10-replace_command_with_function/index.js
+++ 10-replace_command_with_function/index.js
@@ -4,12 +4,18 @@ class ChargeCalculate {
     this._usage = usage;
     this._provider = provider;
   }
+
   get baseCharge() {
     return this._customer.baseRate * this._usage;
   }
+
   get charge() {
     return this.baseCharge + this._provider.connectionCharge;
   }
 }
 
-export default ChargeCalculate;
+function charge(customer, usage, provider) {
+  return new ChargeCalculate(customer, usage, provider).charge
+}
+
+export default charge;
