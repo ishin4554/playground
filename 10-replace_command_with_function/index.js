9cd2ac9243bf7d2efec2029fef09f6662f394543
minw <ishin4554@gmail.com>
2023-02-03 15:15:35 +0800

    REFACTOR remove property

--- 10-replace_command_with_function/index.js
+++ 10-replace_command_with_function/index.js
@@ -1,18 +1,12 @@
 class ChargeCalculate {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-
   charge(customer, usage, provider) {
-    const baseCharge = this._customer.baseRate * this._usage;
-    return baseCharge + this._provider.connectionCharge;
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
   }
 }
 
 function charge(customer, usage, provider) {
-  return new ChargeCalculate(customer, usage, provider).charge(customer, usage, provider)
+  return new ChargeCalculate().charge(customer, usage, provider)
 }
 
 export default charge;
