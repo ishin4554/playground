87f7cbc5a460c04a89070378b63a63117670d3f5
minw <ishin4554@gmail.com>
2023-02-03 14:28:02 +0800

    UPDATE create factory function

--- 08-replace_constructor_with_factory_function/__tests__/index.test.js
+++ 08-replace_constructor_with_factory_function/__tests__/index.test.js
@@ -1,8 +1,8 @@
-import Employee from "../index";
+import createEmployee from "../index";
 
 describe('replace constructor with factory function', function () {
   test('should be Engineer', function () {
-    const candidate = new Employee('Terry', 'E')
+    const candidate = createEmployee('Terry', 'E')
     expect(candidate.type).toBe('Engineer');
   });
 });
--- 08-replace_constructor_with_factory_function/index.js
+++ 08-replace_constructor_with_factory_function/index.js
@@ -9,4 +9,7 @@ class Employee {
     return { "E": "Engineer", "M": "Manager", "S": "Salesman" }
   }
 }
-export default Employee;
+function createEmployee(name, typeCode) {
+  return new Employee(name, typeCode)
+}
+export default createEmployee;
